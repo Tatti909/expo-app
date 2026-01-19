6d6ebabadc2c2683fc346066ee7d289e7d827e4b
bhm240@myy.haaga-helia.fi <bhm240@myy.haaga-helia.fi>
2026-01-19 20:55:52 +0200

    Laskin

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState(null);
+
+  const sum = () => {
+    setResult(Number(num1) + Number(num2));
+  };
+
+  const subtract = () => {
+    setResult(Number(num1) - Number(num2));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.resultText}>
+        Result: {result !== null ? result : ''}
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={setNum1}
+        placeholder="First number"
+      />
+
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={num2}
+        onChangeText={setNum2}
+        placeholder="Second number"
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button title="+" onPress={sum} />
+        <Button title="-" onPress={subtract} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +51,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F6A7F6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    width: 150,
+    marginbottom: 10,
+    padding: 5,
+  },
+  resultText:{
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  }
 });
